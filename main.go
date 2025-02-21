7a42976aaf3d72691d29fec0e4b673717efeb4a8
Christina <christina123danubia@gmail.com>
2025-02-20 21:25:23 -0300

    Add files via upload

--- main.go
+++ main.go
@@ -14,14 +14,53 @@ func abreIndex(resposta http.ResponseWriter, requisicao *http.Request){
 	page.Execute(resposta, nil)
 }
 
+func abreCadastro(resposta http.ResponseWriter, requisicao *http.Request){
+	page, erro := template.ParseFiles("template/cadastro.html")
+
+	if erro != nil {
+		fmt.Println("Erro em cadastro", erro)
+		return
+	}
+	page.Execute(resposta, nil)
+}
+
+func salvaDoador(resposta http.ResponseWriter, requisicao *http.Request){
+	page, erro := template.ParseFiles("template/cadastro.html")
+	
+	if erro != nil {
+		fmt.Println("Erro em cadastro", erro)
+		return
+	}
+	
+
+	if requisicao.Method == http.MethodPost{
+	nomeDoador := requisicao.FormValue("nome")
+	telefoneDoador := requisicao.FormValue("telefone")
+	emailDoador := requisicao.FormValue("email")
+	tipoDoador := requisicao.FormValue("tipo")
+
+	fmt.Println("O nome do Doador é: ", nomeDoador)
+	fmt.Println("O telefone do Doador é: ", telefoneDoador)
+	fmt.Println("O email do Doador é: ", emailDoador)
+	fmt.Println("O tipo do Doador é: ", tipoDoador)
+
+	}
+	page.Execute(resposta, nil)
+}
+
+
+
+
 func main() {
 	//Criando um EndPoint
-	http.HandleFunc("/", abreIndex)
+	http.HandleFunc("/inicial", abreIndex)
+	http.HandleFunc("/cadastro", abreCadastro)
+	http.HandleFunc("/salvadoador", salvaDoador)
 
+	fmt.Print("Iniciando Servidor!!!!")
 	//Subindo o servidor na porta 8080
 	erro := http.ListenAndServe(":8080", nil)
 	if erro != nil {
 		fmt.Print("Servidor com Problemas")
 	}
-	fmt.Print("ohayou sekai good morning world");
 }
